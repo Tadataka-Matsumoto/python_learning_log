1584d4e7ea7c64ccad6b8ecf3be726c933d77f24
Tadataka-Matsumoto <tdtk1538@gmail.com>
2021-01-19 10:22:54 +0900

    環境変数に基づくDEBUGの設定

--- learing_log/settings.py
+++ learing_log/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ydj_c8sz83)1wz^t&b$3amkzzgxxgsffekllohotv!m%ot&2ae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True#p282によりオフにしてみる
 
 ALLOWED_HOSTS = []
 
